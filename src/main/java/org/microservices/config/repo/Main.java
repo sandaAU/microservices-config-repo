b8832241d4223dccfb8985f00fab906225b56ee9
vladimirkacer <vladimir.kacer@epcomm.cz>
2023-07-09 18:01:45 +0200

    ADD PROPERTY FILES 2

--- src/main/java/org/microservices/config/repo/Main.java
+++ src/main/java/org/microservices/config/repo/Main.java
@@ -2,8 +2,46 @@ package org.microservices.config.repo;
 
 
 /**
- *
+ * WE DONT USE THIS MAIN CLASS, WE USE ONLY PROPERTIES AS PROJECT
+ * 1) department-service.properties
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservices-SpringCloudConfigServer_javaguides.net_2022_10_RameshFadatare_8.7.23-17.45\microservices-config-repo\src\main\resources\department-service.properties
  * 
+ * 2) 
+ * user-service.properties
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservices-SpringCloudConfigServer_javaguides.net_2022_10_RameshFadatare_8.7.23-17.45\microservices-config-repo\src\main\resources\ user-service.properties
+ * 
+ * THIS PROJECT 
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservices-SpringCloudConfigServer_javaguides.net_2022_10_RameshFadatare_8.7.23-17.45\microservices-config-repo\src\main\java\org\microservices\config\repo
+ * ID PUSHED TO GITGUB
+ * microservices-config-repo Public
+ * https://github.com/sandaAU/microservices-config-repo
+ * https://github.com/sandaAU/microservices-config-repo.git
+ * 
+ * AND PROPERTIES department-service.properties AND user-service.properties IN THIS PROJECTS org.microservices.config.repo
+ * ARE LOADED FROM GITHUB 
+ * https://github.com/sandaAU/microservices-config-repo
+ * AS PROPERTIES TO PROJETCS WITHIN CONFIG SERVER
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservices-SpringCloudConfigServer_javaguides.net_2022_10_RameshFadatare_8.7.23-17.45\SpringCloudConfigServer
+ * 
+ * PROPERTIES
+ * 1) department-service.properties FOR PROJECT /DepartmentService: 
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservicesCommunicationExampleUsing-RestTemplate_javaguides.net_2022-10_RameshFadatare_2.7.23-23.30\DepartmentService
+ * 2) user-service.properties FOR PROJECT /UserService2:
+ * F:\17.7.21-13.55\PROJECTS\ES6.6.22-23.16\SpringBoot-SpringBootMicroservicesCommunicationExampleUsing-RestTemplate_javaguides.net_2022-10_RameshFadatare_2.7.23-23.30\UserService2
+ * 
+ * IT'S MEAN, THAT PROJECTS /DepartmentService AND /UserService2 ARE NOT LOADED LOCAL PROPERTIES STORED IN PROJECTS, BUT ARE LOADED
+ * PROPERTIES FROM GITHUB https://github.com/sandaAU/microservices-config-repo.git
+ * 
+ * BECAUSE OF IT WAS CHANGED PASSWORD TO ROOT USER MYSQL 
+ * PSW MYSQL CHANGED 8.7.23-21.17
+ * FOR MySQL Server 8.0
+ * How to Reset or Change MySQL Root Password on Linux or Windows
+ * ==============================================================
+ * October 21, 2018
+ * https://phoenixnap.com/kb/how-to-reset-mysql-root-password-windows-linux
+ * mysql> ALTER USER 'root'@'localhost' IDENTIFIED BY 'Hiuy%R68^eDs#0*jJ7&78';
+ * Query OK, 0 rows affected (0.06 sec)
+ * mysql>
  */
 public class Main {
     
